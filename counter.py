3d5e2d1be3f528fbcac535dd97f53b69ed1a1440
Taylor Moog <taylormoog@vu.com>
2023-04-03 21:00:48 -0500

    used line.startswith('From'), then assigned sender=words[1], then added sender to d with a counter, then assigned a bigsender and bigcount

--- counter.py
+++ counter.py
@@ -3,6 +3,32 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+    d=dict()
+    for line in handle:
+        line=line.rstrip()
+        words=line.split()
+        if line.startswith('From'):
+            if line.startswith('From:'):
+                continue
+            else:
+                sender = words[1]
+                if sender not in d:
+                    d[sender] = 1
+                else:
+                    d[sender] += 1
+    BigSender = None
+    BigCount = 0
+    for key in d:
+        if d[key] >= BigCount:
+            BigCount = d[key]
+            BigSender = key
+    print(BigSender, BigCount)
+    
+    
+
+
+
+
         
 
 ## if you want to test locally run > python counter.py
